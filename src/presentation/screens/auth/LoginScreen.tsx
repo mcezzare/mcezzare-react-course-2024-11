31a652d22d03117d3208973b7a4da387c4fa0fdf
Mario Cezzare <mario.chiodi@catho.com>
2024-11-18 22:37:07 -0300

    install axios and create http adpter

--- src/presentation/screens/auth/LoginScreen.tsx
+++ src/presentation/screens/auth/LoginScreen.tsx
@@ -4,13 +4,11 @@ import { MyIcon } from '../../components/ui/MyIcon';
 import { StackScreenProps } from '@react-navigation/stack';
 import { RootStackParams } from '../../navigation/StackNavigator';
 
-import { API_URL, STAGE } from '@env';
 interface Props extends StackScreenProps<RootStackParams, 'LoginScreen'> { }
 
 export const LoginScreen = ( { navigation }: Props ) => {
   const { height } = useWindowDimensions();
 
-  console.log( { apiUrl: API_URL, stage: STAGE } )
 
   return (
     <Layout style={ {
