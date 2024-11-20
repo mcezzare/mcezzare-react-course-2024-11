a1d9e5ef1401772724f3217b402340d1f46d0782
Mario Cezzare <mario.chiodi@catho.com>
2024-11-20 01:00:30 -0300

    create debug curl

--- src/config/api/tesloApi.tsx
+++ src/config/api/tesloApi.tsx
@@ -1,16 +1,14 @@
 import { STAGE, API_URL as PROD_URL, API_URL_IOS, API_URL_ANDROID } from '@env';
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { Platform } from 'react-native';
 import { StorageAdapter } from '../storage/storage-adapter';
 
-
 export const API_URL =
-  ( STAGE === 'prod' )
+  STAGE === 'prod'
     ? PROD_URL
     : Platform.OS === 'ios'
       ? API_URL_IOS
       : API_URL_ANDROID;
-
 
 const tesloApi = axios.create( {
   baseURL: API_URL,
@@ -19,23 +17,43 @@ const tesloApi = axios.create( {
   },
 } );
 
-console.log( tesloApi.getUri() );
+// Generate curl command
+const generateCurlCommand = ( config: AxiosRequestConfig ): string => {
+  const { method, url, headers, data, baseURL } = config;
+  let curl = `curl -X ${ method?.toUpperCase() } '${ baseURL }${ url }'`;
 
-// TODO: interceptors
-tesloApi.interceptors.request.use(
-  async ( config ) => {
-
-    const token = await StorageAdapter.getItem( 'token' );
-    if ( token ) {
-      config.headers[ 'Authorization' ] = 'Bearer $token';
+  // Add headers to curl
+  if ( headers ) {
+    for ( const [ key, value ] of Object.entries( headers ) ) {
+      curl += ` -H '${ key }: ${ value }'`;
     }
-
-
-    return config;
   }
 
-);
-console.log( tesloApi.options );
-export {
-  tesloApi,
+  // Add body to request, if exists
+  if ( data ) {
+    curl += ` -d '${ typeof data === 'string' ? data : JSON.stringify( data ) }'`;
+  }
+
+  return curl;
 };
+
+// Interceptors
+tesloApi.interceptors.request.use(
+  async ( config ) => {
+    // Add token to headers, if exists
+    const token = await StorageAdapter.getItem( 'token' );
+    if ( token ) {
+      config.headers[ 'Authorization' ] = `Bearer ${ token }`;
+    }
+
+    // Log curl \o/
+    console.debug( 'Request CURL:', generateCurlCommand( config ) );
+
+    return config;
+  },
+  ( error ) => {
+    return Promise.reject( error );
+  }
+);
+
+export { tesloApi };
