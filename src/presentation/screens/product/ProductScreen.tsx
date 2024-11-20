5cd8035c9bb53a885545cb934706dcb5911e3c16
Mario Cezzare <mario.chiodi@catho.com>
2024-11-20 18:58:44 -0300

    wip product screen

--- src/presentation/screens/product/ProductScreen.tsx
+++ src/presentation/screens/product/ProductScreen.tsx
@@ -1,18 +1,20 @@
-import { Input, Layout } from '@ui-kitten/components';
+import { Button, ButtonGroup, Input, Layout, Text, useTheme } from '@ui-kitten/components';
 import { MainLayout } from '../../layouts/MainLayout';
 import { getProductsById } from '../../../actions/products/get-product-by-id';
 import { useQuery } from '@tanstack/react-query';
 import { StackScreenProps } from '@react-navigation/stack';
 import { RootStackParams } from '../../navigation/StackNavigator';
 import React, { useRef } from 'react';
-import { FlatList, ScrollView } from 'react-native';
+import { FlatList } from 'react-native';
 import { FadeInImage } from '../../components/ui/FadeInImage';
+import { ScrollView } from 'react-native-gesture-handler';
+import { Gender, Size } from '../../../domain/entities/products';
 
 interface Props extends StackScreenProps<RootStackParams, 'ProductScreen'> { }
 
 export const ProductScreen = ( { route }: Props ) => {
   const productIdRef = useRef( route.params.productId );
-
+  const theme = useTheme();
   // console.log( route.productId );
   // useQuery
   // useMutation
@@ -27,6 +29,10 @@ export const ProductScreen = ( { route }: Props ) => {
     return ( <MainLayout title=" Loading..." /> );
   }
 
+  // list to components multi choice
+  const sizes: Size[] = [ Size.Xs, Size.Xxl, Size.Xl, Size.L, Size.M, Size.L ];
+
+  const genders: Gender[] = [ Gender.Kid, Gender.Men, Gender.Unisex, Gender.Women ];
 
   return (
     <MainLayout
@@ -90,10 +96,75 @@ export const ProductScreen = ( { route }: Props ) => {
               value={ product.stock.toString() }
               style={ { marginVertical: 5, flex: 1 } }
             />
-
-
           </Layout>
 
+          {/* Selectors */ }
+          <Layout>
+            <Text
+              style={ {
+                margin: 2,
+                marginTop: 5,
+                marginHorizontal: 15,
+                fontWeight: 'bold'
+              }
+              }>Sizes</Text>
+            {/* Sizes */ }
+            <ButtonGroup
+              style={ { margin: 2, marginTop: 15, marginHorizontal: 5 } }
+              size="small"
+              appearance="outline"
+            >
+              {
+                sizes.map( ( size, index ) => (
+                  <Button
+                    key={ size }
+                    style={ {
+                      flex: 1,
+                      backgroundColor: false ? theme[ 'color-primary-200' ] : undefined,
+                    } }
+                  >{ size }
+
+
+                  </Button>
+                ) )
+              }
+
+            </ButtonGroup>
+
+
+
+            {/* Gender */ }
+            <Text
+              style={ {
+                margin: 2,
+                marginTop: 5,
+                marginHorizontal: 15,
+                fontWeight: 'bold'
+              }
+              }>Genders</Text>
+
+            <ButtonGroup
+              style={ { margin: 2, marginTop: 16, marginHorizontal: 5 } }
+              size="small"
+              appearance="outline"
+            >
+              {
+                genders.map( ( gender, index ) => (
+                  <Button
+                    key={ gender }
+                    style={ {
+                      flex: 1,
+                      backgroundColor: false ? theme[ 'color-primary-200' ] : undefined,
+                    } }
+                  >{ gender }
+
+
+                  </Button>
+                ) )
+              }
+
+            </ButtonGroup>
+          </Layout> 
 
 
 
