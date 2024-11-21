d4646131788c301c8fb8ca2aff96a9736418199a
Mario Cezzare <mario.chiodi@catho.com>
2024-11-21 19:46:58 -0300

    refactoring

--- src/presentation/screens/product/ProductScreen.tsx
+++ src/presentation/screens/product/ProductScreen.tsx
@@ -1,19 +1,26 @@
-import { Button, ButtonGroup, Input, Layout, Text, useTheme } from '@ui-kitten/components';
+import { useRef } from 'react';
+import {
+  ButtonGroup,
+  Input,
+  Layout,
+  Button,
+  useTheme,
+} from '@ui-kitten/components';
+import { Formik } from 'formik';
+
 import { MainLayout } from '../../layouts/MainLayout';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import { ScrollView } from 'react-native-gesture-handler';
 import { StackScreenProps } from '@react-navigation/stack';
 import { RootStackParams } from '../../navigation/StackNavigator';
 
-import { updateCreateProduct, getProductsById } from '../../../actions/products/';
+import { updateCreateProduct, getProductsById } from '../../../actions/products';
 
-import { useRef } from 'react';
-import { Product } from '../../../domain/entities/products';
+import { ScrollView } from 'react-native-gesture-handler';
 import { MyIcon } from '../../components/ui/MyIcon';
-import { Formik } from 'formik';
 
 import { ProductImages } from '../../components/products/ProductImages';
-import { genders, sizes } from '../../../config/constants/product.constants';
+import { Product } from '../../../domain/entities/products';
+import { sizes, genders } from '../../../config/constants/product.constants';
 
 interface Props extends StackScreenProps<RootStackParams, 'ProductScreen'> { }
 
@@ -22,209 +29,150 @@ export const ProductScreen = ( { route }: Props ) => {
   const theme = useTheme();
   const queryClient = useQueryClient();
 
-  // useQuery
   const { data: product } = useQuery( {
-    queryKey: [ 'products', productIdRef.current ],
-    staleTime: 1000 * 60 * 1, // 1 minutes
+    queryKey: [ 'product', productIdRef.current ],
     queryFn: () => getProductsById( productIdRef.current ),
-
   } );
 
-  // useMutation
   const mutation = useMutation( {
-    mutationFn: ( data: Product ) => updateCreateProduct( { ...data, id: productIdRef.current } ),
+    mutationFn: ( data: Product ) =>
+      updateCreateProduct( { ...data, id: productIdRef.current } ),
     onSuccess( data: Product ) {
-      productIdRef.current = data.id; //  will be used in creating
-      queryClient.invalidateQueries( { queryKey: [ 'products', 'infinite' ] } );
+      productIdRef.current = data.id; // creación
 
-      // queryClient.invalidateQueries( { queryKey: [ 'product', productIdRef.current ] } );
-      // other way without invalidate all cache
-      queryClient.setQueryData( [ 'product', productIdRef.current ], data );
+      queryClient.invalidateQueries( { queryKey: [ 'products', 'infinite' ] } );
+      queryClient.invalidateQueries( { queryKey: [ 'product', data.id ] } );
+      // queryClient.setQueryData(['product',  data.id ], data);
     },
   } );
 
-
   if ( !product ) {
-    return ( <MainLayout title=" Loading..." /> );
+    return <MainLayout title="Loading..." />;
   }
 
-  // list to components multi choice
-  // const sizes: Size[] = [ Size.Xs, Size.S, Size.M, Size.L, Size.Xl, Size.Xxl ];
-
-  // const genders: Gender[] = [ Gender.Kid, Gender.Men, Gender.Unisex, Gender.Women ];
-
   return (
-    <Formik
-      initialValues={ product }
-      // onSubmit={ values => mutation.mutate(values) }
-      onSubmit={ mutation.mutate }
-    >
-      {
-        ( { handleChange, handleSubmit, values, errors, setFieldValue } ) => (
-          <MainLayout
-            title={ values.title }
-            subTitle={ `Price : ${ values.price }` }
-          >
-            <ScrollView style={ { flex: 1 } }>
-              {/* /Images */ }
-              <Layout style={ {
-                marginVertical: 10, justifyContent: 'center', alignItems: 'center',
+    <Formik initialValues={ product } onSubmit={ mutation.mutate }>
+      { ( { handleChange, handleSubmit, values, errors, setFieldValue } ) => (
+        <MainLayout title={ values.title } subTitle={ `Precio: ${ values.price }` }>
+          <ScrollView style={ { flex: 1 } }>
+            {/* product images */ }
+            <Layout
+              style={ {
+                marginVertical: 10,
+                justifyContent: 'center',
+                alignItems: 'center',
               } }>
+              <ProductImages images={ values.images } />
+            </Layout>
 
-                <ProductImages
-                  images={ values.images }
-                />
+            {/* FORM */ }
+            <Layout style={ { marginHorizontal: 10 } }>
+              <Input
+                label="Title"
+                style={ { marginVertical: 5 } }
+                value={ values.title }
+                onChangeText={ handleChange( 'title' ) }
+              />
+              <Input
+                label="Slug"
+                value={ values.slug }
+                onChangeText={ handleChange( 'slug' ) }
+                style={ { marginVertical: 5 } }
+              />
+              <Input
+                label="Description"
+                value={ values.description }
+                onChangeText={ handleChange( 'description' ) }
+                multiline
+                numberOfLines={ 5 }
+                style={ { marginVertical: 5 } }
+              />
+            </Layout>
 
-                {/* /Forms */ }
+            {/* Price & Stock */ }
+            <Layout
+              style={ {
+                marginVertical: 5,
+                marginHorizontal: 15,
+                flexDirection: 'row',
+                gap: 10,
+              } }>
+              <Input
+                label="Precio"
+                value={ values.price.toString() }
+                onChangeText={ handleChange( 'price' ) }
+                style={ { flex: 1 } }
+                keyboardType="numeric"
+              />
 
-                <Layout style={ { marginHorizontal: 10 } }>
-                  <Input
-                    label="Title"
-                    value={ values.title }
-                    style={ { marginVertical: 5 } }
-                    onChangeText={ handleChange( 'title' ) }
-                  />
-                  <Input
-                    label="SLUG"
-                    value={ values.slug }
-                    style={ { marginVertical: 5 } }
-                    onChangeText={ handleChange( 'slug' ) }
-                  />
-                  <Input
-                    label="Description"
-                    multiline
-                    numberOfLines={ 5 }
-                    value={ values.description }
-                    style={ { marginVertical: 5 } }
-                    onChangeText={ handleChange( 'description' ) }
-                  />
-                </Layout>
+              <Input
+                label="Inventario"
+                value={ values.stock.toString() }
+                onChangeText={ handleChange( 'stock' ) }
+                style={ { flex: 1 } }
+                keyboardType="numeric"
+              />
+            </Layout>
 
-                {/* Price and Stock */ }
-                <Layout style={ { marginVertical: 5, marginHorizontal: 15, flexDirection: 'row', gap: 10 } }>
+            {/* Selectores */ }
+            <ButtonGroup
+              style={ { margin: 2, marginTop: 20, marginHorizontal: 15 } }
+              size="small"
+              appearance="outline">
+              { sizes.map( size => (
+                <Button
+                  onPress={ () =>
+                    setFieldValue(
+                      'sizes',
+                      values.sizes.includes( size )
+                        ? values.sizes.filter( s => s !== size )
+                        : [ ...values.sizes, size ],
+                    )
+                  }
+                  key={ size }
+                  style={ {
+                    flex: 1,
+                    backgroundColor: values.sizes.includes( size )
+                      ? theme[ 'color-primary-200' ]
+                      : undefined,
+                  } }>
+                  { size }
+                </Button>
+              ) ) }
+            </ButtonGroup>
 
-                  <Input
-                    label="Price"
-                    value={ values.price.toString() }
-                    style={ { marginVertical: 5, flex: 1 } }
-                    onChangeText={ handleChange( 'price' ) }
-                    keyboardType="numeric"
-                  />
+            <ButtonGroup
+              style={ { margin: 2, marginTop: 20, marginHorizontal: 15 } }
+              size="small"
+              appearance="outline">
+              { genders.map( gender => (
+                <Button
+                  onPress={ () => setFieldValue( 'gender', gender ) }
+                  key={ gender }
+                  style={ {
+                    flex: 1,
+                    backgroundColor: values.gender.startsWith( gender )
+                      ? theme[ 'color-primary-200' ]
+                      : undefined,
+                  } }>
+                  { gender }
+                </Button>
+              ) ) }
+            </ButtonGroup>
 
-                  <Input
-                    label="Inventory"
-                    value={ values.stock.toString() }
-                    style={ { marginVertical: 5, flex: 1 } }
-                    onChangeText={ handleChange( 'stock' ) }
-                    keyboardType="number-pad"
-                  />
-                </Layout>
+            {/* Save button */ }
+            <Button
+              accessoryLeft={ <MyIcon name="save-outline" white /> }
+              onPress={ () => handleSubmit() }
+              disabled={ mutation.isPending }
+              style={ { margin: 15 } }>
+              Save
+            </Button>
 
-                {/* Selectors */ }
-                <Layout>
-                  <Text
-                    style={ {
-                      margin: 2,
-                      marginTop: 5,
-                      marginHorizontal: 15,
-                      fontWeight: 'bold',
-                    }
-                    }>Sizes</Text>
-                  {/* Sizes */ }
-                  <ButtonGroup
-                    style={ { margin: 2, marginTop: 15, marginHorizontal: 5 } }
-                    size="small"
-                    appearance="outline"
-                  >
-                    {
-                      sizes.map( ( size, index ) => (
-                        <Button
-                          onPress={ () => setFieldValue(
-                            'sizes',
-                            values.sizes.includes( size )
-                              ? values.sizes.filter( s => s !== size )
-                              : [ ...values.sizes, size ]
-                          ) }
-                          key={ size }
-                          style={ {
-                            flex: 1,
-                            backgroundColor: values.sizes.includes( size )
-                              ? theme[ 'color-primary-200' ]
-                              : undefined,
-                          } }
-                        >{ size }
-
-
-                        </Button>
-                      ) )
-                    }
-
-                  </ButtonGroup>
-
-
-
-                  {/* Gender */ }
-                  <Text
-                    style={ {
-                      margin: 2,
-                      marginTop: 5,
-                      marginHorizontal: 15,
-                      fontWeight: 'bold',
-                    }
-                    }>Genders</Text>
-
-                  <ButtonGroup
-                    style={ { margin: 2, marginTop: 16, marginHorizontal: 5 } }
-                    size="small"
-                    appearance="outline"
-                  >
-                    {
-                      genders.map( ( gender, index ) => (
-                        <Button
-                          onPress={ () => setFieldValue( 'gender', gender ) }
-                          key={ gender }
-                          style={ {
-                            flex: 1,
-                            backgroundColor: values.gender.startsWith( gender )
-                              ? theme[ 'color-primary-200' ]
-                              : undefined,
-                          } }
-                        >{ gender }
-
-
-                        </Button>
-                      ) )
-                    }
-
-                  </ButtonGroup>
-
-
-                  {/* Save Button */ }
-                  <Button
-                    onPress={ () => handleSubmit() }
-                    style={ {
-                      marginTop: 24,
-                      margin: 8,
-                    } }
-                    accessoryLeft={ <MyIcon name="save-outline" white /> }
-                    disabled={ mutation.isPending }
-                  >Save item
-                  </Button>
-
-                </Layout>
-
-                {/* <Text>{ JSON.stringify( values, null, 2 ) }</Text> */ }
-
-              </Layout>
-              {/* Fix ios bottom inacessible    */ }
-              <Layout style={ { height: 200 } } />
-
-            </ScrollView>
-
-          </MainLayout>
-
-        )
-      }
+            <Layout style={ { height: 200 } } />
+          </ScrollView>
+        </MainLayout>
+      ) }
     </Formik>
   );
 };
