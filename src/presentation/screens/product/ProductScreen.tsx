3f9390fd4f80bcbdbf4bbbe293a9a1ad9c5b2e40
Mario Cezzare <mario.chiodi@catho.com>
2024-11-21 23:12:54 -0300

    end 333 - add icon to photos

--- src/presentation/screens/product/ProductScreen.tsx
+++ src/presentation/screens/product/ProductScreen.tsx
@@ -53,7 +53,13 @@ export const ProductScreen = ( { route }: Props ) => {
   return (
     <Formik initialValues={ product } onSubmit={ mutation.mutate }>
       { ( { handleChange, handleSubmit, values, errors, setFieldValue } ) => (
-        <MainLayout title={ values.title } subTitle={ `Precio: ${ values.price }` }>
+        <MainLayout
+          title={ values.title }
+          subTitle={ `Precio: ${ values.price }` }
+          rightAction={ () => console.log( 'camera' ) }
+          rightActionIcon="camera-outline"
+        >
+
           <ScrollView style={ { flex: 1 } }>
             {/* product images */ }
             <Layout
